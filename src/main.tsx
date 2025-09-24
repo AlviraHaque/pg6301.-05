6d9170eafafd1de7b3180b283d4b50a5a9448049
alvir <alha041@kristiania.student.no>
2025-09-24 13:14:03 +0200

    har en fungerende applikasjon

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,37 @@
 import { createRoot } from "react-dom/client";
-import React, { useState } from "react";
+import React, { type FormEvent, useState } from "react";
 
 function Application() {
   const [tasks, setTasks] = useState([
-    { description: "something" },
-    { description: " something else" },
+    { title: "my task" },
+    { title: "my other task" },
   ]);
-  return <h1>My App</h1>;
+
+  const [title, setTitle] = useState("hei");
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    const task = { title };
+    setTasks((oldTasks) => [task, ...oldTasks]);
+  }
+
+  return (
+    <>
+      <h1>My App</h1>
+      <ul>
+        {tasks.map((t) => (
+          <li>{t.title}</li>
+        ))}
+      </ul>
+      <h2>Add new tasks</h2>
+      <form onSubmit={handleSubmit}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        <div>
+          <button>Add </button>
+        </div>
+      </form>
+    </>
+  );
 }
 
 createRoot(document.getElementById("app")!).render(<Application />);
